3fbd9e95f11ed31a7a52df4b4bab7ce8d4cb45cb
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-03-11 21:10:36 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,27 +3,16 @@ import math
 
 def build_heap(data):
     swaps = []
-    n = len(data)
 
-    while n != 0:
-        (data[int(math.floor((n-1)/2))], data[n-1]) = (data[n-1], data[int(math.floor((n-1)/2))])
+    for i in reversed(data)
+        n = data.index(i)
 
-        swaps.append((int(math.floor((n-1)/2)), n))
+        while n != 0:
+            if data[n] < data[int(math.floor((n-1)/2))]:
+                data[n],data[int(math.floor((n-1)/2)))]=data[int(math.floor((n-1)/2))],data[n]
+                swaps.append((int(math.floor((n-1)/2)), n))
 
-        n1 = n
-
-        while 2*n1 + 1 < len(data): #WHICH N1 SHOULD BE???????
-            if data[n1] > data[2*n1]:
-                (data[2*n1], data[n1]) = (data[n1], data[2*n1]) 
-                
-
-
-                
-                swaps.append((n1, 2*n1))
-                
-                n1 = 2*n1
-        
-        n = int(math.floor((n-1)/2)) 
+            n = int(math.floor((n-1)/2)) 
  
 
 
@@ -41,7 +30,7 @@ def main():
         file_name = input()
         file = "./test/" + file_name
         with open(file) as f:
-            т = int(f.readline())
+            n = int(f.readline())
             data = list(map(int, f.readline().split()))
     if 'I' in text:
         n = int(input())
