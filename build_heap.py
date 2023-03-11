11f56a079e57a05946cc3d77b914959cc485a725
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-03-11 21:21:12 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -4,22 +4,15 @@ import math
 def build_heap(data):
     swaps = []
 
-    for i in reversed(data)
+    for i in reversed(data):
         n = data.index(i)
 
         while n != 0:
             if data[n] < data[int(math.floor((n-1)/2))]:
-                data[n],data[int(math.floor((n-1)/2)))]=data[int(math.floor((n-1)/2))],data[n]
+                data[n],data[int(math.floor((n-1)/2))]=data[int(math.floor((n-1)/2))],data[n]
                 swaps.append((int(math.floor((n-1)/2)), n))
 
             n = int(math.floor((n-1)/2)) 
- 
-
-
-
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
 
     return swaps
 
@@ -36,23 +29,13 @@ def main():
         n = int(input())
         data = list(map(int, input().split()))
 
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
